535861724cfa96419b731db3b96f9ccaff61ee24
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-05-02 19:40:17 -0400

    created show page and added css

--- controllers/breads.js
+++ controllers/breads.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const Bread = require('../models/bread')
+const Bread = require('../models/breads')
 
 // GET all the bread
 router.get('/', (req, res) => {
@@ -11,7 +11,11 @@ router.get('/', (req, res) => {
 // GET bread a specific bread
 router.get('/:index', (req, res) =>{
   const { index } = req.params
-  res.send(Bread[index])
+  res.render('show', {
+    bread: Bread[index]
+  })
 })
 
 module.exports = router
+
+// get, post, put, patch, delete
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
 require('dotenv').config()
-const breadController = require('./controllers/bread')
+const express = require('express')
+const breadController = require('./controllers/breads')
 
 const app = express()
 
 // middleware
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
